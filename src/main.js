ce73679303a37d47ef11204d2df3513c2815cbf2
RafaelLeone <rinaciodacosta@gmail.com>
2022-09-08 14:57:47 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import App from "./App.vue";
+import HoMe from "./pages/HoMe.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(HoMe),
+}).$mount("#home");
---
+++ src/pages/HoMe.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <TarefaList msg="Welcome to Your Vue.js Home" :tasks="listaDeTarefas" />
+    <p>{{ listaDeTarefas }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import TarefaList from "../components/TarefaList.vue";
+
+export default {
+  name: "HoMe",
+  components: {
+    TarefaList,
+  },
+  data: () => {
+    return {
+      listaDeTarefas: ["A", "B", "C"],
+    };
+  },
+  created() {
+    console.log("terminei de carregar a página");
+    axios.get("http://localhost:3000/tasks/").then((response) => {
+      console.log(response.data);
+      this.listaDeTarefas = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+#home {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
